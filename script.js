82ff4f7cc679739cc0a74d8a4f0dd96404d496d5
Devin McDaniel <dmcdaniel9@gmail.com>
2023-02-28 16:12:25 +0000

    bug fixes
    Added error catching and refactored

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 'use strict'
 
-    const quoteButton = document.querySelector("quote-button");
-    const quoteText = document.querySelector("quote-text");
-    const quoteAuthor = document.querySelector("quote-author");
+const quoteButton = document.querySelector('.quote-button');
+const quoteText = document.querySelector('.quote-text');
+const quoteAuthor = document.querySelector('.quote-author');
+let quotesReceived = false;
 
-    async function getNewQuote() {
-      let req = await fetch("https://stoic-server.herokuapp.com/search/searchterm");
-      let processedReq = await res.json()
+
+async function newQuoteRequest() {
+   try {
+      let req = await fetch('https://stoicquotesapi.com/v1/api/quotes', {
+          method: "GET",
+          headers: {
+              "Content-Type": "application/json",
+          }
+      });
+      let processedReq = await req.json();
       
-      let res = processedReq[0].data
+      let x = Math.floor(Math.random() * 10)
+      let res = processedReq.data[x];
+      
+      quoteText.textContent = res.body
+      quoteAuthor.textContent = res.author
+      
+   } catch (err) {
+      err = "Error"
+      console.log(err);
+   }
+}
 
-      console.log(res)
-    }
 
-    quoteButton.addEventListener('click', getNewQuote);
+quoteButton.addEventListener('click', newQuoteRequest)
